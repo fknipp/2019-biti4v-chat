928289cfefc3050849d0cda6db0b2d80c00b833d
Franz Knipp <franz@qnipp.com>
2019-05-22 11:38:03 +0200

    Adding accounts-ui for login

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
+import AccountsUIWrapper from './AccountsUIWrapper.js';
 
 const App = () => (
   <div>
+    <AccountsUIWrapper />
     <h1>Welcome to Meteor!</h1>
     <Hello />
     <Info />
