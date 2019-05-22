c88588cf590058fb44f45735f8c1af2bd82c38d3
Franz Knipp <franz@qnipp.com>
2019-05-22 10:46:35 +0200

    Changing Hello component to functional style

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -1,22 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
-export default class Hello extends Component {
-  state = {
-    counter: 0,
-  }
-
-  increment() {
-    this.setState({
-      counter: this.state.counter + 1
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={() => this.increment()}>Click Me</button>
-        <p>You've pressed the button {this.state.counter} times.</p>
-      </div>
-    );
-  }
+export default () => {
+  const [ counter, setCounter ] = useState(0);
+  return (
+    <button onClick={() => setCounter(c => c + 1)}>
+      {counter} Mal geklickt
+    </button>
+  );
 }
